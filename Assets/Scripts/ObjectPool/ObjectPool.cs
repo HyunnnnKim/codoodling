cbbaef594a0471b25d1515eab3e30274f9971015
hyunnnnkim <hwkim931224@gmail.com>
2021-03-09 12:10:06 +0900

    added object pool
    needs changes with singleton

---
+++ Assets/Scripts/ObjectPool/ObjectPool.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Collie
+{
+    public class ObjectPool : MonoBehaviour
+    {
+        #region Singleton
+        public static ObjectPool Instance = null;
+        #endregion
+
+        #region Serialize Field
+        [Space, Header("Pools")]
+        [SerializeField]
+        private List<Pool> referencePools = null;
+        #endregion
+
+        #region Private Field
+        private Dictionary<string, Queue<GameObject>> _objPools = null;
+        #endregion
+
+        private void Awake()
+        {
+            CreateInstance();
+            InitVariables();
+        }
+
+        #region Initialize
+        private void CreateInstance()
+        {
+            if (Instance == null) Instance = this;
+        }
+
+        private void InitVariables()
+        {
+            _objPools = new Dictionary<string, Queue<GameObject>>();
+        }
+        #endregion
+
+        private void Start()
+        {
+            CreatePools();
+        }
+
+        #region Create Pools
+        private void CreatePools()
+        {
+            foreach (Pool pool in referencePools)
+            {
+                Queue<GameObject> objectPool = new Queue<GameObject>();
+
+                for (int i = 0; i < pool.Size; i++)
+                {
+                    // Pick random prefab from the prefab list.
+                    int randomIndex = Random.Range(0, pool.Prefabs.Count - 1);
+                    GameObject obj = Instantiate(pool.Prefabs[randomIndex]);
+
+                    obj.SetActive(false);
+                    objectPool.Enqueue(obj);
+                }
+                _objPools.Add(pool.Tag.ToString(), objectPool);
+            }
+        }
+        #endregion
+
+        #region Controls
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <param name="position"></param>
+        /// <param name="rotation"></param>
+        /// <returns></returns>
+        public GameObject Spawn(string tag, Vector3 position, Quaternion rotation)
+        {
+            if (!_objPools.ContainsKey(tag) || _objPools[tag].Count <= 0) return null;
+            
+            GameObject obj2Spawn = _objPools[tag].Dequeue();
+            obj2Spawn.SetActive(true);
+            obj2Spawn.transform.position = position;
+            obj2Spawn.transform.rotation = rotation;
+
+            return obj2Spawn;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <param name="gameObject"></param>
+        public void Terminate(string tag, GameObject gameObject)
+        {
+            gameObject.SetActive(false);
+            _objPools[tag].Enqueue(gameObject);
+        }
+        #endregion
+    }
+
+    [System.Serializable]
+    public class Pool
+    {
+        #region Serialize Field
+        [Space, Header("Pool")]
+        [SerializeField]
+        private string tag = null;
+        [SerializeField]
+        private List<GameObject> prefabs = null;
+        [SerializeField]
+        private int size = 0;
+        #endregion
+
+        #region Properties
+        public string Tag { get => tag; set => tag = value; }
+        public List<GameObject> Prefabs { get => prefabs; set => prefabs = value; }
+        public int Size { get => size; set => size = value; }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/ObjectPool/RandomSpawner.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace Collie
+{
+    public class RandomSpawner : MonoBehaviour
+    {
+        #region Serialize Field
+        [Space, Header("Object to spawn")]
+        [SerializeField]
+        private string spawnObjectTag = null;
+        [SerializeField]
+        private int spawnLimit = 10;
+        [SerializeField, Range(1, 10)]
+        private float delayTime = 3f;
+        #endregion
+
+        #region Private Field
+        private Vector3 _randomPoint = Vector3.zero;
+        private float _elapsedTime = Mathf.Infinity;
+        private float _waitTime = 0;
+        private int _numberOfSpawnedObjects = 0;
+        private Color _gizmosColor = new Color(0.3f, 0f, 0f, 0.6f);
+        #endregion
+
+        private void Update()
+        {
+            if (_numberOfSpawnedObjects < spawnLimit)
+            {
+                _elapsedTime += Time.deltaTime;
+                if (_elapsedTime < _waitTime) return;
+
+                CalculateRandomPoint(ref _randomPoint);
+                PointSpawn(spawnObjectTag, _randomPoint);
+
+                _waitTime = Mathf.Clamp(Random.Range(delayTime - 1, delayTime + 1), 1, 10);
+                _elapsedTime = 0f;
+            }
+        }
+
+        #region Spawn
+        private void CalculateRandomPoint(ref Vector3 randomPoint)
+        {
+            var origin = transform.position;
+            var range = transform.localScale / 2.0f;
+            var randomRange = new Vector3(
+                Random.Range(-range.x, range.x),
+                Random.Range(-range.y, range.y),
+                Random.Range(-range.z, range.z));
+            randomPoint = origin + randomRange;
+        }
+
+        private void PointSpawn(string tag, Vector3 point)
+        {
+            ObjectPool.Instance.Spawn(tag, point, Quaternion.identity);
+        }
+        #endregion
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = _gizmosColor;
+            Gizmos.DrawCube(transform.position, transform.localScale);
+        }
+    }
+}
