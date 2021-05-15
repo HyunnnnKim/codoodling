4f42551ef78b7a8290b579b9df5bebeb81f11c9b
David Kim <hwkim931224@gmail.com>
2021-05-15 22:38:58 +0900

    Organize

--- Assets/ProjectInfo/Scripts/ProjectReadme.cs
+++ Assets/ProjectInfo/Scripts/ProjectReadme.cs
@@ -5,30 +5,28 @@ using UnityEngine;
 public class ProjectReadme : ScriptableObject
 {
     #region Serialized Field
-    
-    [SerializeField] private Texture2D icon;
+    [SerializeField] private Texture2D icon = null;
     public Texture2D Icon => icon;
     
-    [SerializeField] private string title;
+    [SerializeField] private string title = null;
     public string Title => title ?? "NULL";
     
-    [SerializeField] private Section[] sections;
+    [SerializeField] private Section[] sections = null;
     public Section[] Sections => sections;
 
-    [SerializeField] private bool loadedLayout;
+    [SerializeField] private bool loadedLayout = false;
     public bool LoadedLayout { get => loadedLayout; set => loadedLayout = value; }
 
-    [SerializeField] private Font font;
+    [SerializeField] private Font font = null;
     public Font Font => font;
-    
     #endregion
 
     [Serializable]
     public class Section
     {
-        public string heading;
-        public string text;
-        public string linkText;
-        public string url;
+        public string heading = null;
+        public string text = null;
+        public string linkText = null;
+        public string url = null;
     }
 }
