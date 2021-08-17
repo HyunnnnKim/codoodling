89babfeff3f1da8eeeeac89ceb57d09dd22765ab
David Kim <hwkim931224@gmail.com>
2021-08-17 23:50:46 +0900

    COD-16 Testing out SpriteRenderer...

--- Assets/Scripts/DesignPattern/CommandPattern/Gameboy/BackgroundMovement.cs
+++ Assets/Scripts/DesignPattern/CommandPattern/Gameboy/BackgroundMovement.cs
@@ -7,7 +7,7 @@ public class BackgroundMovement : MonoBehaviour
 {
     #region Serialized Field
     [SerializeField] private float scrollSpeed = 6f;
-    [SerializeField] private List<Image> backgroundLayer = null;
+    [SerializeField] private List<SpriteRenderer> backgroundLayer = null;
     #endregion
 
     public Vector2 scrollVec = Vector2.zero;
@@ -18,7 +18,7 @@ public class BackgroundMovement : MonoBehaviour
         {
             scrollVec.x += scrollSpeed * Time.deltaTime;
             layer.material.mainTextureOffset = scrollVec;
-            //mat.SetVector("_ScrollSpeed", scrollVec);
+            //layer.material.SetVector("_ScrollSpeed", new Vector2(scrollSpeed, 0f));
         }
     }
 }
