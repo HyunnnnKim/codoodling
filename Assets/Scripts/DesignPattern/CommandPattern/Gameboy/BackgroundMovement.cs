cdc92b35b444301a9722c5baf1929632937310cc
David Kim <hwkim931224@gmail.com>
2021-08-19 00:19:51 +0900

    COD-16 🔨 Fixing mask issue

--- Assets/Scripts/DesignPattern/CommandPattern/Gameboy/BackgroundMovement.cs
+++ Assets/Scripts/DesignPattern/CommandPattern/Gameboy/BackgroundMovement.cs
@@ -10,7 +10,7 @@ public class BackgroundMovement : MonoBehaviour
     #endregion
 
     #region Private Field
-    public Vector2 scrollVec = Vector2.zero;
+    private Vector2 scrollVec = Vector2.zero;
     #endregion
 
     private void Update()
@@ -18,7 +18,8 @@ public class BackgroundMovement : MonoBehaviour
         foreach (var layer in backgroundLayer)
         {
             scrollVec.x += scrollSpeed * Time.deltaTime;
-            layer.material.SetTextureOffset("_MainTex", scrollVec);
+            /* using layer.material won't work if the Image component is child to Mask Component. */
+            layer.materialForRendering.SetTextureOffset("_MainTex", scrollVec);
         }
     }
 }
